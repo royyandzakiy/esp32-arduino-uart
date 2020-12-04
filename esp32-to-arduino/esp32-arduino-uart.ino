b945264e683f58ed401b1f328d08232c70207be2
royyandzakiy <royyan.dzakiy@efishery.com>
2020-12-04 19:11:45 +0700

    inital commit

--- esp32-to-arduino/esp32-arduino-uart.ino
+++ esp32-to-arduino/esp32-arduino-uart.ino
@@ -7,18 +7,16 @@
 HardwareSerial SerialArduino(2);
 
 void sendUARTPayload() {
-  SerialArduino.write("efishery_000FD\n");
-  SerialArduino.write("4-HS7SuQG958lQ\n");
-  SerialArduino.write("1606731546\n");
+  SerialArduino.write("text1\n");
+  SerialArduino.write("text2\n");
+  SerialArduino.write("text3\n");
 }
 
 void setup() {
     Serial.begin(115200);
     Serial.println("ESP32 Ready!");
 
-    SerialArduino.begin(115200, SERIAL_8N1, ESP32_RX2, ESP32_TX2);
-    SerialArduino.write("Hello, Friend! - from ESP32\n");
-    sendUARTPayload();
+    SerialArduino.begin(115200, SERIAL_8N1, ESP32_RX2, ESP32_TX2); SerialArduino.write("Hello, Friend! - from ESP32\n"); sendUARTPayload();
 }
 
 void loop() {
